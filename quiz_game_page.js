b428389ce5995f2b838049ee2362066f4a4f753c
trazman69 <79809003+trazman69@users.noreply.github.com>
2021-05-30 18:41:12 -0700

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -54,4 +54,12 @@ function check() {
         question_turn="player1";
         document.getElementById("player_question").innerHTML="Question Turn - " + player1_name;
     }
+    if(answer_turn=="player1"){
+        answer_turn="player2";
+        document.getElementById("player_answer").innerHTML="Answer Turn - " + player2_name;
+     }
+     else{
+         answer_turn="player1";
+         document.getElementById("player_answer").innerHTML="Answer Turn - " + player1_name;
+     }
 }
