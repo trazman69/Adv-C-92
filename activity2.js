08221bb8fd1b8ad6d2d7c10bc8e8520ca599c888
trazman69 <79809003+trazman69@users.noreply.github.com>
2021-05-30 18:52:23 -0700

    Delete activity2.js

--- activity2.js
+++
@@ -1,9 +0,0 @@
-function addUser() {
-  player1_name = document.getElementById("player1_name_input").value;
-  player2_name = document.getElementById("player2_name_input").value;
-
-  localStorage.setItem("player1name", player1_name);
-  localStorage.setItem("player2name", player2_name);
-
-  window.location = "quiz_game_page.html";
-}
